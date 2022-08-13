1facff34321b4d1dd1f82d6cbcf30b2b95fb5b79
Piero Enriquez <pieroenriquezvera@gmail.com>
2022-08-12 22:08:06 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const OPTIONS = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '50dbf79dbdmsh9f56621d589cff7p181123jsn5f358c9fa59d',
+		'X-RapidAPI-Host': 'ip-geolocation-and-threat-detection.p.rapidapi.com'
+	}
+}
+
+const fetchIpInfo = ip => {
+    return fetch(`https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`, OPTIONS)
+        .then(res => res.json())
+        .catch(err => console.log(err))
+}
+
+const form = document.querySelector('#form')
+const input = document.querySelector('#input')
+const submit = document.querySelector('#submit')
+const results = document.querySelector('#results')
+
+form.addEventListener('submit', async (event) => {
+    event.preventDefault()
+    const {value} = input
+    if (!value) return
+
+    submit.setAttribute('disabled', '')
+    submit.setAttribute('aria-busy', 'true')
+
+    const ipInfo = await fetchIpInfo(value)
+
+    if (ipInfo) {
+        results.innerHTML = JSON.stringify(ipInfo, null, 2)
+    }
+
+    submit.removeAttribute('disabled')
+    submit.removeAttribute('aria-busy')
+
+})
